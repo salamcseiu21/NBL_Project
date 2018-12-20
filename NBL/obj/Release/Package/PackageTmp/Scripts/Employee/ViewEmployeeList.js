ebfaa7cd69b8383e7bfd0f10b9a9d40f66c34ccd
Abdus Salam <salamcseiu21@gmail.com>
2018-12-20 16:17:56 +0600

    Code Modification

---
+++ NBL/obj/Release/Package/PackageTmp/Scripts/Employee/ViewEmployeeList.js
@@ -0,0 +1,41 @@
+﻿
+$.ajax({
+    url: '/editor/employee/GetAllEmployee',
+    type: "GET",
+    datatype: "JSON",
+    success: function (data) {
+        $('#table_employee_list').dataTable({
+            data: data,
+            responsive: true,
+            "order": [[ 0, "desc" ]],
+            columns: [
+                { 'data': 'EmployeeNo' },
+                { 'data': 'EmployeeName' },
+                { 'data': 'Department.DepartmentName' },
+                { 'data': 'Designation.DesignationName' },
+                {
+                    'data': 'PresentAddress'
+                    
+                },
+                {
+                    'data': 'Phone'
+
+                },
+                {
+                    'data': 'Email'
+
+                },
+                {
+                    'data': 'JoiningDate',
+                    className: "text-center",
+                    'render': function (jsonDate) {
+                        var date = new Date(parseInt(jsonDate.substr(6)));
+                        var month = date.getMonth() + 1;
+                        return date.getDate() + "-" + month + "-" + date.getFullYear();
+                    }
+                }
+            ]
+        });
+    }
+});
+
